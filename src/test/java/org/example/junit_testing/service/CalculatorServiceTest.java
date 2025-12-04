c7fb8092473c2c4799b0b438b575e105b7edc178
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-12-04 23:59:39 +0530

    JUnit 4 Testing

--- src/test/java/org/example/junit_testing/service/CalculatorServiceTest.java
+++ src/test/java/org/example/junit_testing/service/CalculatorServiceTest.java
@@ -1,4 +1,26 @@
 package org.example.junit_testing.service;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CalculatorServiceTest {
+
+
+
+    //test methods
+    @Test
+    public void addTest(){
+        int result = CalculatorService.add(12,13);
+        int expected = 25;
+
+        Assert.assertEquals(expected,result);
+    }
+
+    @Test
+    public void sumTest(){
+        int result = CalculatorService.sum(1,2,3);
+        int expected = 6;
+
+        Assert.assertEquals(expected,result);
+    }
 }
